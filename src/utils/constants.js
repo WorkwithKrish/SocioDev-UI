f622c2677f757e8d4bae6173c1a505b7de9cbef2
Krishna <manifester2@outlook.com>
2025-09-26 20:21:46 +0530

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
-    ? "http://localhost:3000"
-    : "https://devkp.xyz";
+    ? "http://localhost:3000/api"
+    : "https://devkp.xyz/api";
